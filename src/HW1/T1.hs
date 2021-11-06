de4316a20c82dbf6f59164ad0ac0843e1ca741ee
nikitamann <45663507+nikitamann@users.noreply.github.com>
2021-11-06 16:33:15 +0300

    Update T1.hs

--- src/HW1/T1.hs
+++ src/HW1/T1.hs
@@ -3,22 +3,32 @@ module HW1.T1 where
 import GHC.Natural (Natural)
 -- | 1. Define the following data type:
 -- | data Day = Monday | Tuesday | ... | Sunday
-data Day = Monday
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Show, Eq)
 
 -- | 2. Implement the following functions:
 
 -- | Returns the day that follows the day of the week given as input.
 nextDay :: Day -> Day
-nextDay = undefined
+nextDay Monday = Tuesday
+nextDay Tuesday = Wednesday
+nextDay Wednesday = Thursday
+nextDay Thursday = Friday
+nextDay Friday = Saturday
+nextDay Saturday = Sunday
+nextDay Sunday = Monday
 
 -- | Returns the day of the week after a given number of days has passed.
 afterDays :: Natural -> Day -> Day
-afterDays = undefined
+afterDays 0 day = day
+afterDays passed day = afterDays (passed - 1) (nextDay day)
 
 -- | Checks if the day is on the weekend.
 isWeekend :: Day -> Bool
-isWeekend = undefined
+isWeekend Saturday = True
+isWeekend Sunday = True
+isWeekend _ = False
 
 -- | Computes the number of days until the next Friday.
 daysToParty :: Day -> Natural
-daysToParty = undefined
+daysToParty Friday = 0
+daysToParty day = daysToParty (nextDay day) + 1
