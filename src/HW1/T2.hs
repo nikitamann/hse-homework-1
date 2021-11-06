33779ec285cbcbaad9701eec74ec7b0b3403f63a
nikitamann <45663507+nikitamann@users.noreply.github.com>
2021-11-06 19:39:48 +0300

    Update T2.hs

--- src/HW1/T2.hs
+++ src/HW1/T2.hs
@@ -8,22 +8,44 @@ data Nat = Z | S Nat
 -- | 1. Implement the following operations
 
 nplus :: Nat -> Nat -> Nat        -- addition
-nplus = undefined
+nplus Z x = x
+nplus (S x) y = S (nplus x y)
 
 nmult :: Nat -> Nat -> Nat       -- multiplication
-nmult = undefined
+nmult x Z = Z
+nmult x (S y) = nplus x (nmult x y)
 
 nsub :: Nat -> Nat -> Maybe Nat   -- subtraction     (Nothing if result is negative)
-nsub = undefined
+nsub Z x = Just x
+nsub x Z = Nothing
+nsub  (S x) (S y) = nsub x y
 
 ncmp :: Nat -> Nat -> Ordering  -- comparison      (Do not derive Ord)
-ncmp = undefined
+ncmp Z Z = EQ
+ncmp Z _ = LT
+ncmp _ Z = GT
+ncmp (S x) (S y) = ncmp x y
 
 nFromNatural :: Natural -> Nat
-nFromNatural = undefined
+nFromNatural 0 = Z
+nFromNatural x = S (nFromNatural (x-1))
 
 nToNum :: Num a => Nat -> a
-nToNum = undefined
+nToNum Z = 0
+nToNum (S x) = (1 + nToNum x)
+
+-- | 2*. Implement the following operations
+-- (Advanced) Implement the following operations:
+
+nEven, nOdd :: Nat -> Bool    -- parity checking
+nEven = undefined
+nOdd = undefined
+
+ndiv :: Nat -> Nat -> Nat         -- integer division
+ndiv = undefined
+
+nmod :: Nat -> Nat -> Nat         -- modulo operation
+nmod = undefined
 
 -- | 2*. Implement the following operations
 -- (Advanced) Implement the following operations:
