70fca1957eb667d1f2181ee423344636e14710c3
nikitamann <45663507+nikitamann@users.noreply.github.com>
2021-11-07 12:07:27 +0300

    Update T1.hs

--- src/HW1/T1.hs
+++ src/HW1/T1.hs
@@ -9,26 +9,26 @@ data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday 
 
 -- | Returns the day that follows the day of the week given as input.
 nextDay :: Day -> Day
-nextDay Monday = Tuesday
-nextDay Tuesday = Wednesday
+nextDay Monday    = Tuesday
+nextDay Tuesday   = Wednesday
 nextDay Wednesday = Thursday
-nextDay Thursday = Friday
-nextDay Friday = Saturday
-nextDay Saturday = Sunday
-nextDay Sunday = Monday
+nextDay Thursday  = Friday
+nextDay Friday    = Saturday
+nextDay Saturday  = Sunday
+nextDay Sunday    = Monday
 
 -- | Returns the day of the week after a given number of days has passed.
 afterDays :: Natural -> Day -> Day
-afterDays 0 day = day
+afterDays 0 day      = day
 afterDays passed day = afterDays (passed - 1) (nextDay day)
 
 -- | Checks if the day is on the weekend.
 isWeekend :: Day -> Bool
 isWeekend Saturday = True
-isWeekend Sunday = True
-isWeekend _ = False
+isWeekend Sunday   = True
+isWeekend _        = False
 
 -- | Computes the number of days until the next Friday.
 daysToParty :: Day -> Natural
 daysToParty Friday = 0
-daysToParty day = daysToParty (nextDay day) + 1
+daysToParty day    = daysToParty (nextDay day) + 1
